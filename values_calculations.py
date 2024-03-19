704f5d2594b0b2023f7e25a22c275b86fde15215
Mateus Mengatto <65681163+mateusmengatto@users.noreply.github.com>
2024-03-19 12:54:33 -0300

    Delete values_calculations.py

--- values_calculations.py
+++
@@ -1,5 +0,0 @@
-from search_data import *
-
-def sum_expenses(data): #if data is filtered get filter function
-    expenses = data.loc[data['Valor'] < 0]
-    summed_expenses = sum(expenses['Valor'])
